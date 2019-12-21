0193667d1ea07dc3f92db46bd80c2648687a808e
Tien <viettienha98@gmail.com>
2019-12-21 11:47:37 +0700

    Follow and unfollow

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative 'boot'
-
+require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -11,8 +11,12 @@ module RubyApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # Include the authenticity token in remote forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
