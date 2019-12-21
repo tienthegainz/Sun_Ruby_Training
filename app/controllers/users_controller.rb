1d231d1a541b57205d9ecc75ae61b9a707458e3d
Tien <viettienha98@gmail.com>
2019-12-21 10:42:42 +0700

    Micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    # debugger
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
@@ -57,13 +57,13 @@ class UsersController < ApplicationController
     end
 
     # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+    # def logged_in_user
+    #   unless logged_in?
+    #     store_location
+    #     flash[:danger] = "Please log in."
+    #     redirect_to login_url
+    #   end
+    # end
     
     # Confirms the correct user.
     def correct_user
